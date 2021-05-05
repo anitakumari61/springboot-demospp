8fac40d2d865f0eb5fe8c298ccf8dcac3698fcf5
anitakumari61 <anitachoudhary1823@gmail.com>
2021-05-05 19:39:16 +0530

    First Commit

---
+++ src/main/java/com/example/restservices/SpringbootDemoappApplication.java
@@ -0,0 +1,13 @@
+package com.example.restservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoappApplication.class, args);
+	}
+
+}
